9dcb238def810a9ee8201774f32e74aa29904411
Zixuan Zhang <39664230+HakkerBarry@users.noreply.github.com>
2022-04-11 17:06:38 -0600

    add simple shader

--- Final/SimpleFS.glsl
+++ Final/SimpleFS.glsl
@@ -1,8 +1,12 @@
 #version 330 core
+
+in vec3 w_normal;
+
 out vec4 FragColor;
 
 void main()
 {
     //FragColor = diffuse + ref;
-    FragColor = vec4(1, 1, 1, 0);
+    FragColor = vec4(gl_position, 0);
+    FragColor = vec4(w_normal, 0);
 } 
--- Final/SimpleVS.glsl
+++ Final/SimpleVS.glsl
@@ -1,10 +1,14 @@
 #version 330 core
 layout (location = 0) in vec3 m_pos;
-layout (location = 1) in vec3 normal;
+layout (location = 1) in vec3 m_normal;
+
+out vec3 w_normal;
 
 uniform mat4 mvp;
+unifrom mat4 m;
 
 void main()
 {
 	gl_Position = mvp * vec4(m_pos, 1.0);
+	w_normal = inverse(transpose(mat3(m))) * m_normal;
 }
