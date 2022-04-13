e51575a9d728d4d493a8565e3f9eb107c8f3d90d
Zixuan Zhang <39664230+HakkerBarry@users.noreply.github.com>
2022-04-12 23:45:12 -0600

    g Buffer

--- Final/SimpleFS.glsl
+++ Final/SimpleFS.glsl
@@ -3,13 +3,15 @@
 in vec3 w_normal;
 in vec3 frag_pos;
 
-out vec4 FragColor;
+layout (location = 0) out vec4 FragColor;
+layout (location = 1) out vec3 gNormal;
+layout (location = 2) out vec4 gAlbedo;
 
 void main()
 {
     //FragColor = diffuse + ref;
-    //FragColor = vec4(frag_pos.xy, 0, 0);
+    FragColor = vec4(frag_pos.xy, 0, 0);
     //vec3 normal = normalize(w_normal);
-    //FragColor = vec4(normal, 1);
-    FragColor = vec4(vec3(0.95), 1);
+    gNormal = normalize(w_normal);
+    gAlbedo.rgb = vec3(0.95);
 } 
